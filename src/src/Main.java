96d64df5a84d2332697ab1aa3f66e506d162e46d
LUCAS VICENTE DA SILVA <lv249697@gmail.com>
2025-08-25 16:02:19 -0300

    Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -17,6 +17,55 @@ public class Main {
                 default:
 
             }
-        }while (sairDoApp != 3);
+                 case 3:
+                        if (usuario == null) {
+                            System.out.println("Cadastre um usuário primeiro!\n");
+                            break;
+                        }
+                        catalogo.exibirCatalogo();
+                        System.out.print("Digite o título da mídia para adicionar: ");
+                        String tituloAdd = sc.nextLine();
+                        System.out.print("Digite o nome da playlist: ");
+                        String nomePL = sc.nextLine();
+
+                        Playlist p = usuario.getPlaylist(nomePL);
+                        if (p != null) {
+                            for (Midias m : catalogo.midias) {
+                                if (m.getTitulo().equalsIgnoreCase(tituloAdd)) {
+                                    p.adicionarMidia(m);
+                                    System.out.println("Mídia adicionada!\n");
+                                }
+                            }
+                        } else {
+                            System.out.println("Playlist não encontrada!\n");
+                        }
+                        break;
+
+                    case 4:
+                        if (usuario == null) {
+                            System.out.println("Cadastre um usuário primeiro!\n");
+                            break;
+                        }
+                        System.out.print("Nome da playlist: ");
+                        String nomeRemover = sc.nextLine();
+                        Playlist pr = usuario.getPlaylist(nomeRemover);
+                        if (pr != null) {
+                            System.out.print("Título da mídia a remover: ");
+                            String tituloRemover = sc.nextLine();
+                            pr.removerMidia(tituloRemover);
+                            System.out.println("Mídia removida!\n");
+                        } else {
+                            System.out.println("Playlist não encontrada!\n");
+                        }
+                        break;
+
+                    case 5:
+                        if (usuario == null) {
+                            System.out.println("Cadastre um usuário primeiro!\n");
+                            break;
+                        }
+                        usuario.exibirPlaylists();
+                        break;
+
+        }
     }
-}
