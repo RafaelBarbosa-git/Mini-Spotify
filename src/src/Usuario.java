463f39b01c70d71165406e6393e984baed3e1c67
LUCAS VICENTE DA SILVA <lv249697@gmail.com>
2025-08-25 16:00:07 -0300

    Update Usuario.java

--- src/src/Usuario.java
+++ src/src/Usuario.java
@@ -19,4 +19,26 @@ public class Usuario {
     public String toString() {
         return "Nome: " + nome + "   ;  email: " + email;
     }
+      public void criarPlaylist(String nomePlaylist) {
+        playlists.add(new Playlist(nomePlaylist));
+    }
+
+    public Playlist getPlaylist(String nome) {
+        for (Playlist p : playlists) {
+            if (p != null && p.toString().contains(nome)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void exibirPlaylists() {
+        if (playlists.isEmpty()) {
+            System.out.println("Nenhuma playlist criada ainda.");
+        } else {
+            for (Playlist p : playlists) {
+                p.exibirPlaylist();
+            }
+        }
+    }
 }
