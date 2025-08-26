ed44a20fa5f5f47dc0106499959c6c7b14f96ee5
LUCAS VICENTE DA SILVA <lv249697@gmail.com>
2025-08-25 23:01:20 -0300

    Update Midias.java

--- src/src/Midias.java
+++ src/src/Midias.java
@@ -1,4 +1,4 @@
-public class Midias {
+
     private String titulo;
     private String artista;
     private String duracao;
@@ -18,7 +18,7 @@ public class Midias {
         return artista;
     }
 
-    public double getDuracao() {
+    public String getDuracao() {
         return duracao;
     }
 
@@ -30,4 +30,3 @@ public class Midias {
     public String toString() {
         return titulo + " - " + artista + " (" + duracao + " min, " + genero + ")";
     }
-}
