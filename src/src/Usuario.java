e6274700944b14e3e2ea3e0d1473d50ec21d2ff1
LUCAS VICENTE DA SILVA <lv249697@gmail.com>
2025-08-25 22:59:03 -0300

    Update Usuario.java

--- src/src/Usuario.java
+++ src/src/Usuario.java
@@ -1,44 +1,29 @@
+import java.util.ArrayList;
+
 public class Usuario {
     private String nome;
     private String email;
+    private ArrayList<Playlist> playlists = new ArrayList<>();
 
     public Usuario(String nome, String email) {
         this.nome = nome;
         this.email = email;
     }
 
-    public String getNome() {
-        return nome;
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+
+    public void adicionarPlaylist(Playlist playlist) {
+        playlists.add(playlist);
     }
 
-    public String getEmail() {
-        return email;
+    public ArrayList<Playlist> getPlaylists() {
+        return playlists;
     }
 
     @Override
     public String toString() {
-        return "Nome: " + nome + "   ;  email: " + email;
-    }
-      public void criarPlaylist(String nomePlaylist) {
-        playlists.add(new Playlist(nomePlaylist));
-    }
-
-    public Playlist getPlaylist(String nome) {
-        for (Playlist p : playlists) {
-            if (p != null && p.toString().contains(nome)) {
-                return p;
-            }
-        }
-        return null;
-    }
-
-    public void exibirPlaylists() {
-        if (playlists.isEmpty()) {
-            System.out.println("Nenhuma playlist criada ainda.");
-        } else {
-            for (Playlist p : playlists) {
-                p.exibirPlaylist();
-            }
-        }
+        return "Usuário: " + nome + " (" + email + ")";
     }
 }
+
